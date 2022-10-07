d79a9f5ee0087d9a7d1ff97f4afdb2017620d1ef
= <azazasdfg.com@gmail.com>
2022-10-07 20:33:24 +0300

    More cleaning & configuration

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
