7f5dff8209973e7c04213ce4568a223f5087247c
= <azazasdfg.com@gmail.com>
2022-10-10 00:46:32 +0300

    Add margin for IconButtons after 800px width

--- src/components/Container/Container.styled.ts
+++ src/components/Container/Container.styled.ts
@@ -4,12 +4,17 @@ const StyledContainer = styled.div`
 	max-width: 400px;
 	margin: 0 auto;
 	padding: 0 0.5em;
-
+	
 	@media (max-width: 360px) {
 		& {
 			font-size: 4.4vw;
 		}
-		
+	}
+	
+	@media (min-width: 800px) {
+		& {
+			max-width: 600px;
+		}
 	}
 `
 
--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -3,8 +3,22 @@ import styled from 'styled-components';
 export const StyledHeader = styled.header`
 	font-size: 0.875em;
 	max-width: 400px;
-	margin: 1.5em 0;
+	margin: 1.5em auto;
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
+
+	@media (min-width: 800px) {
+		& {
+			font-size: 1.1em;
+			max-width: 530px;
+		}
+
+		& button:nth-child(2) {
+			margin-left: 0.6em;
+		}
+		& button:nth-last-child(2) {
+			margin-right: 0.6em;
+		}
+	}
 `
