a1f02d4a2d9285596de6edc6856f78d0bf75a131
= <azazasdfg.com@gmail.com>
2022-10-09 13:40:34 +0300

    Fix Header font-size

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-	font-size: 14px;
+	font-size: 0.875em;
 	max-width: 400px;
-	margin: 1.5em auto;
-	padding: 0 0.5em;
+	margin: 1.5em 0;
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
