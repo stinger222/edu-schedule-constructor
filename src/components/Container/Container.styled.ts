a8a8a1bde7d361ac4630c5dc7dc6f128b54a060d
= <azazasdfg.com@gmail.com>
2022-10-10 19:55:40 +0300

    Change Container possition and logic a bit

--- src/components/Container/Container.styled.ts
+++ src/components/Container/Container.styled.ts
@@ -3,17 +3,19 @@ import styled from "styled-components";
 const StyledContainer = styled.div`
 	max-width: 400px;
 	margin: 0 auto;
-	padding: 0 0.5em;
 	
-	@media (max-width: 360px) {
+	@media (max-width: 400px) {
 		& {
-			font-size: 4.4vw;
+			font-size: 3.83vw;
+			padding: 0 0.5em
 		}
 	}
-	
+
 	@media (min-width: 800px) {
 		& {
-			max-width: 600px;
+			padding: 0;
+			max-width: 650px;
+			font-size: 1.625em;
 		}
 	}
 `
--- src/components/pages/Main/Main.tsx
+++ src/components/pages/Main/Main.tsx
@@ -1,15 +1,22 @@
-import Container from "../../Container/Container"
 import Header from "../../Header/Header"
+import LessonCard from "../../LessonCard/LessonCard"
 import StyledMain from "./Main.styled"
 
 const Main = () => {
 	return (
-		<Container>
-			<StyledMain>
-				<Header/>
-				<div className="card">Card</div>
-			</StyledMain>
-		</Container>
+		<StyledMain>
+			<Header/>
+
+			<LessonCard
+				cabinet="302т"
+				startTime="10:10"
+				endTime="11:10"
+				lessonId={1}
+				lessonName="Какая-то хуйня"
+				teacherName="Какой-то хуй"
+			/>
+
+		</StyledMain>
 	)
 }
 
