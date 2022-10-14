b1be9163758d65fc56944304640908ed38c506b7
= <azazasdfg.com@gmail.com>
2022-10-14 21:08:42 +0300

    Fix a little Header re-rendering issue

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,19 @@
-import { memo } from "react";
 import HeaderButton from "../HeaderButton/HeaderButton";
 import IconButton from "../IconButton/IconButton";
 import { StyledHeader } from "./Header.styled";
+import { memo } from "react";
 
- const Header: React.FC = memo(() => {	
+const Header: React.FC = memo(() => {
 	return (
 		<StyledHeader>
-			<IconButton
-				iconPath={require('../../assets/schedules-icon.png')}
-			/>
+			<IconButton iconPath={require('../../assets/schedules-icon.png')} />
 			<HeaderButton id={1}>Пн</HeaderButton>
 			<HeaderButton id={2}>Вт</HeaderButton>
 			<HeaderButton id={3}>Ср</HeaderButton>
 			<HeaderButton id={4}>Чт</HeaderButton>
 			<HeaderButton id={5}>Пт</HeaderButton>
 			<HeaderButton id={6}>Сб</HeaderButton>
-			<IconButton
-				iconPath={require('../../assets/add-icon.png')}
-			/>
+			<IconButton iconPath={require('../../assets/add-icon.png')} />
 		</StyledHeader>
 	)
 })
