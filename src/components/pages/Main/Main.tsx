63ad93552c2c120867b57e62ad62010535dfdcda
= <azazasdfg.com@gmail.com>
2022-10-14 21:09:43 +0300

    Add TEMP implementation of rendering lesson cards

--- src/components/pages/Main/Main.tsx
+++ src/components/pages/Main/Main.tsx
@@ -8,29 +8,44 @@ import LessonCard from "../../LessonCard/LessonCard"
 import StyledMain from "./Main.styled"
 
 const Main = () => {
-	const {lessonsStore} = useContext(StoreContext)
+	const { comsposedSchedulesStore, headerStore, lessonsStore, ringSchedulesStore } = useContext(StoreContext)
+	const selectedDayId = headerStore.selectedDayId
+	const lessons = lessonsStore.lessons
+	const ringSchedules = ringSchedulesStore.schedules
+	const day = comsposedSchedulesStore.schedules[0].week[selectedDayId-1]
+
+	
 	return (
 		<StyledMain>
 			<Header/>
 			<Container>
-			<LessonCard
-				cabinet="302т"
-				startTime="10:10"
-				endTime="11:10"
-				lessonId={1}
-				lessonName="Какая-то хуйня"
-				teacherName="Какой-то хуй"
-				/>
 
-			<BreakCard
-				breakLength={10}
-				startTime="11:10"
-				endTime="11:20"
-			/>
-			<pre>{JSON.stringify(lessonsStore.lessons, null, 2)}</pre>
-			<button onClick={() => {
-				lessonsStore.dick()
-			}}>Click Me</button>
+			{day.lesson_ids.map((lessonId, itemId) => {
+				const lesson = lessons.find(i => i.id === lessonId)
+				const lessonRings = ringSchedules.find(i => i.id === day.ring_schedule_id).lessons[itemId]
+
+				if (!lesson) {
+					return <h1>No lessons with such id!</h1>
+				} else if (!lessonRings) {
+					return <h1>No ring schedules with such id, or this day conains more lessons than defined in selected ring schedule!</h1>
+				}
+
+			return <>
+ 				<LessonCard
+					cabinet={lesson.cabinet}
+					startTime={lessonRings.start}
+					endTime={lessonRings.end}
+					lessonId={itemId + 1}
+					key={itemId}
+					lessonName={lesson.lesson_name}
+					teacherName={lesson.teacher}
+				/>
+				<br/>
+			</>
+			})}
+
+			<pre>{JSON.stringify(comsposedSchedulesStore.schedules[0].week[selectedDayId-1], null, 2)}</pre>
+
 			</Container>
 		</StyledMain>
 	)
