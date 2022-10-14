fd081fc3aa1e510a2dee8dc44bcbe981fb1cef14
= <azazasdfg.com@gmail.com>
2022-10-15 01:03:26 +0300

    Make cards render flow more... readable?
    (нет)

--- src/components/pages/Main/Main.tsx
+++ src/components/pages/Main/Main.tsx
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react-lite"
-import { useContext } from "react"
+import React, { useContext } from "react"
 import { StoreContext } from "../../.."
 import BreakCard from "../../BreakCard/BreakCard"
 import Container from "../../Container/Container"
@@ -9,42 +9,52 @@ import StyledMain from "./Main.styled"
 
 const Main = () => {
 	const { comsposedSchedulesStore, uiStore, lessonsStore, ringSchedulesStore } = useContext(StoreContext)
-	const selectedDayId = uiStore.selectedDayId
+
 	const lessons = lessonsStore.lessons
 	const ringSchedules = ringSchedulesStore.schedules
-	const day = comsposedSchedulesStore.schedules[0].week[selectedDayId-1]
 
+	const selectedDayId = uiStore.selectedDayId
+	const selectedDay = comsposedSchedulesStore.schedules[0].week[selectedDayId-1]
+	const selectedDayRings = ringSchedules.find(i => i.id === selectedDay.ring_schedule_id).lessons
 	
+	// i.e data for certain LessonCard and BreackCard
+	const getCardsData = (lessonId, itemId) => {
+		const lesson = lessons.find(i => i.id === lessonId)
+		const lessonRings = selectedDayRings[itemId]
+		let breakStart = selectedDayRings[itemId]?.end
+		let breakEnd = selectedDayRings[itemId + 1]?.start
+
+		if (itemId + 1 >= selectedDay.lesson_ids.length ) {
+			breakEnd = undefined
+		}
+
+		return [lesson, lessonRings, breakStart, breakEnd]
+	}
+
 	return (
 		<StyledMain>
 			<Header/>
 			<Container>
+				
+			{selectedDay.lesson_ids.map((lessonId, itemId) => {
+				const [ lesson, lessonRings, breakStart, breakEnd ] = getCardsData(lessonId, itemId)
 
-			{day.lesson_ids.map((lessonId, itemId) => {
-				const lesson = lessons.find(i => i.id === lessonId)
-				const lessonRings = ringSchedules.find(i => i.id === day.ring_schedule_id).lessons[itemId]
-
-				if (!lesson) {
-					return <h1>No lessons with such id!</h1>
-				} else if (!lessonRings) {
-					return <h1>No ring schedules with such id, or this day conains more lessons than defined in selected ring schedule!</h1>
-				}
-
-			return <>
- 				<LessonCard
-					cabinet={lesson.cabinet}
-					startTime={lessonRings.start}
-					endTime={lessonRings.end}
-					lessonId={itemId + 1}
-					key={itemId}
-					lessonName={lesson.lesson_name}
-					teacherName={lesson.teacher}
-				/>
-				<br/>
-			</>
+				return <React.Fragment key={itemId}>
+					<LessonCard
+						cabinet={lesson["cabinet"]}
+						startTime={lessonRings["start"]}
+						endTime={lessonRings["end"]}
+						lessonId={itemId + 1}
+						lessonName={lesson["lesson_name"]}
+						teacherName={lesson["teacher"]}
+					/>
+					
+					<BreakCard
+						startTime={breakStart as string | undefined}
+						endTime={breakEnd as string | undefined}
+					/>
+				</React.Fragment>
 			})}
-
-			<pre>{JSON.stringify(comsposedSchedulesStore.schedules[0].week[selectedDayId-1], null, 2)}</pre>
 
 			</Container>
 		</StyledMain>
