f57fd962fad9d73fc1dd9acd814715739884c6d2
= <azazasdfg.com@gmail.com>
2022-10-20 17:14:04 +0300

    Add init routing and create first couple routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { ThemeProvider } from "styled-components";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import GlobalStyles, { theme } from "./components/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import ComposedSchedules from "./components/pages/ComposedSchedules/ComposedSchedules";
 import Main from "./components/pages/Main/Main";
 
 function App() {
   return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
-			<Main/>
+			<HashRouter>
+				<Routes>
+					<Route path="/" element={<Main />}/>
+					<Route path="/composed" element={<ComposedSchedules />}/>
+				</Routes>
+			</HashRouter>
 		</ThemeProvider>
   )
 }
