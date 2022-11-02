55ce80a5d2eee191bac9cfe3b71b2acc528277b8
= <azazasdfg.com@gmail.com>
2022-11-02 17:12:54 +0300

    Make RingSchedules page render stored schedules

--- src/components/pages/RingSchedules/RingSchedules.tsx
+++ src/components/pages/RingSchedules/RingSchedules.tsx
@@ -1,13 +1,28 @@
+import { useContext } from "react"
+import { StoreContext } from "../../.."
 import Container from "../../Container/Container"
 import HeaderSecondary from "../../Header/HeaderSecondary"
 import { StyledRingSchedules } from "./RingSchedules.styled"
 
 const RingSchedules = () => {
+	const { ringSchedulesStore } = useContext(StoreContext)
+
 	return (
 		<StyledRingSchedules>
 			<HeaderSecondary />
 			<Container>
-				<h1>Ring Schedules list will be here</h1>
+				{
+					ringSchedulesStore.schedules.map(schedule => (
+						<div className="schedule_card">
+							<h1 className="schedule_name">{schedule.name}</h1>
+							<div className="schedule_desc">
+								<div><span>Пар:</span><span>{schedule.rings.length}</span></div>
+								<div><span>Начало:</span><span>{schedule.rings[0].start}</span></div>
+								<div><span>Конец:</span><span>{schedule.rings[schedule.rings.length - 1].end}</span></div>
+							</div>
+						</div>
+					))
+				}
 			</Container>
 		</StyledRingSchedules>
 	)
