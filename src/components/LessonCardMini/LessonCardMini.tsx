000b687ecbfc90ca03abe55b40c61bb68ea1549b
stinger222 <azazasdfg.com@gmail.com>
2022-11-04 17:00:11 +0300

    Add future mini version of LessonCard component

---
+++ src/components/LessonCardMini/LessonCardMini.tsx
@@ -0,0 +1,10 @@
+import { StyledLessonCardMini } from "./LessonCardMini.styled"
+
+const LessonCardMini = () => {
+  return <StyledLessonCardMini>
+    <h1>This is StyledLessonCardMini</h1>
+  </StyledLessonCardMini>
+  
+}
+
+export default LessonCardMini
