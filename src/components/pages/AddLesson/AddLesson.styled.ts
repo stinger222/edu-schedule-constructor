33df49375cba17d5a7257fe28f5551c44b81a27d
stinger222 <azazasdfg.com@gmail.com>
2022-11-06 19:25:59 +0300

    Add initial styles for AddLesson page
    This is will be reworked completely dye to form etc

--- src/components/pages/AddLesson/AddLesson.styled.ts
+++ src/components/pages/AddLesson/AddLesson.styled.ts
@@ -1,5 +1,33 @@
 import styled from "styled-components"
 
 export const StyledAddLesson = styled.div`
-  background: red;
+
+  & .wrapper {
+    width: 55%;
+    margin: 0 auto;
+    font-size: 1em;
+  }
+
+  & .lesson_name input {
+    width: 100%;
+    margin-bottom: 0.5em;
+  }
+
+  & .same_line {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  & .teacher_name input {
+    width: 10em;
+  }
+
+   & .cabinet input {
+    width: 3.5em;
+  }
+  
+  & .cabinet, & .teacher_name {
+    display: inline-block;
+  } 
 `
--- src/components/pages/AddLesson/AddLesson.tsx
+++ src/components/pages/AddLesson/AddLesson.tsx
@@ -1,11 +1,36 @@
+import Container from "../../Container/Container"
+import HeaderSecondary from "../../Header/HeaderSecondary"
+import Input from "../../Input/Input"
 import { StyledAddLesson } from "./AddLesson.styled"
 
 const AddLesson = () => {
-  return (
-    <StyledAddLesson>
-      AddLesson
-    </StyledAddLesson>
-  )
+  return <>
+    <HeaderSecondary />
+    <Container>
+      <StyledAddLesson>
+        <div className="wrapper">
+          <Input
+          className="lesson_name"
+          caption="Название пары"
+          placeholder="Уззкий язык"
+          />
+          <div className="same_line">
+
+          <Input
+            className="teacher_name"
+            caption="Имя препода"
+            placeholder="Cтарый хуй"
+            />
+          <Input
+            className="cabinet"
+            caption="Кабинет"
+            placeholder="403у"
+            />
+          </div>
+        </div>
+      </StyledAddLesson>
+    </Container>
+  </>
 }
 
 export default AddLesson
