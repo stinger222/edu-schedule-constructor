5b8b85888020a782a9eb69bf37d45927852c1bce
= <azazasdfg.com@gmail.com>
2022-11-10 00:20:58 +0300

    Make AddLesson form add lesson to the store
    todo: validation

--- src/components/pages/AddLesson/AddLesson.tsx
+++ src/components/pages/AddLesson/AddLesson.tsx
@@ -4,10 +4,13 @@ import HeaderSecondary from "../../Header/HeaderSecondary"
 import Input from "../../Input/Input"
 import { StyledAddLessonPage } from "./AddLesson.styled"
 import * as Yup from "yup"
-import { memo } from "react"
+import { memo, useContext } from "react"
 import ActionButton from "../../ActionButton/ActionButton"
+import { StoreContext } from "../../.."
 
 const AddLesson = () => {
+	const { lessonsStore } = useContext(StoreContext)
+
   const formik = useFormik({
     initialValues: {
       cabinet: '',
@@ -18,7 +21,12 @@ const AddLesson = () => {
       cabinet: Yup.mixed().required('required')
     }),
     onSubmit() {
-      console.log('submitted');
+      console.log('Submitted. \n', formik.values);
+
+			const { cabinet, lesson_name, teacher_name} = formik.values
+			lessonsStore.addLesson(cabinet, teacher_name, lesson_name)
+
+			formik.resetForm()
     }
   })
 
@@ -26,33 +34,33 @@ const AddLesson = () => {
     <HeaderSecondary />
     <Container>
       <StyledAddLessonPage onSubmit={formik.handleSubmit}>
-      <Input
-				id="lesson_name"
-				name="lesson_name"
-				caption="Название пары"
-				placeholder="Уззкий язык"
-				onChange={formik.handleChange}
-				value={formik.values.lesson_name}
-      />
-			<Input
-				id="teacher_name"
-				name="teacher_name"
-				caption="Имя препода"
-				placeholder="Старый хуй"
-				onChange={formik.handleChange}
-				value={formik.values.teacher_name}
-			/>
-			<Input
-				id="cabinet"
-				name="cabinet"
-				caption="Кабинет"
-				placeholder="104п"
-				onChange={formik.handleChange}
-				value={formik.values.cabinet}
-			/>
-			<ActionButton type="submit" className="submit_button">
-				Готово
-			</ActionButton>
+				<Input
+					id="lesson_name"
+					name="lesson_name"
+					caption="Название пары"
+					placeholder="Уззкий язык"
+					onChange={formik.handleChange}
+					value={formik.values.lesson_name}
+				/>
+				<Input
+					id="teacher_name"
+					name="teacher_name"
+					caption="Имя препода"
+					placeholder="Старый хуй"
+					onChange={formik.handleChange}
+					value={formik.values.teacher_name}
+				/>
+				<Input
+					id="cabinet"
+					name="cabinet"
+					caption="Кабинет"
+					placeholder="104п"
+					onChange={formik.handleChange}
+					value={formik.values.cabinet}
+				/>
+				<ActionButton type="submit" className="submit_button">
+					Готово
+				</ActionButton>
       </StyledAddLessonPage>
     </Container>
   </>
