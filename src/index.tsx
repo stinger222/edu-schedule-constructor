46643f1faf0cc57f6e2c94fbc12387a984dc14da
= <azazasdfg.com@gmail.com>
2022-12-15 13:05:55 +0300

    Create init global styles with styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import ReactDOM from 'react-dom/client';
+import GlobalStyles from './core/themes/Global';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+		<GlobalStyles />
+		<App />
   </React.StrictMode>
 );
