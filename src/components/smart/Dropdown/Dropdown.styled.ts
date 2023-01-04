ea1af0edab0f5f8878145992794b748efd087668
stinger222 <azazasdfg.com@gmail.com>
2023-01-05 02:30:47 +0300

    Add some buttons to dropdown

--- src/components/smart/Dropdown/Dropdown.styled.ts
+++ src/components/smart/Dropdown/Dropdown.styled.ts
@@ -1,22 +1,49 @@
 import styled from "styled-components";
 
 export const StyledDropdown = styled.div`
-	--dropdown-font-size: 0.6;
+	--dropdown-font-size: 1.3;
 	--dropdown-margin-top: 1em;
 	
 	font-size: calc(1em * var(--dropdown-font-size));
-
-	height: 17em;
-	width: 30em;
+  font-family: 'JetBrains Mono';
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+	height: fit-content;
+	width: 17em;
+  padding: 0.4em 0.8em;
 	background: white;
-
-	border-radius: 1.5em;
-	box-shadow: 0 0 0 0.15em black, -0.2em 0.5em 0.5em rgb(0 0 0 / 25%);
-
+  
+	border-radius: 1em;
+	box-shadow: 0 0 0 0.08em black, -0.2em 0.3em 0.5em rgb(0 0 0 / 30%);
+  
 	position: absolute;
 	right: 1em;
-
+  
 	/* now even when dropdown font-size will be changed,
-	dropdow position relative to header will be the same */
-	top: calc(1 / var(--dropdown-font-size) * var(--header-height) + var(--dropdown-margin-top)); 	
+	it's position relative to header will be the same */
+	top: calc(1 / var(--dropdown-font-size) * var(--header-height) + var(--dropdown-margin-top));
+  
+  & header {
+    width: 100%;
+    height: 1.5em;
+
+    font-weight: 400;
+    font-size: 1.4em;
+    text-align: center;
+    
+    margin-bottom: 0.5em;
+    border-bottom: 0.08em solid #cdcdcd;
+  }
+  
+  & button {
+    width: 100%;
+    margin-bottom: 0.3em;
+ 
+    font-family: inherit;
+    font-size: 1.2em;
+    font-weight: 300;
+  }
 `
--- src/components/smart/Dropdown/Dropdown.tsx
+++ src/components/smart/Dropdown/Dropdown.tsx
@@ -1,13 +1,17 @@
+import Button from "../../ui/Button/Button"
 import { StyledDropdown } from "./Dropdown.styled"
 
 interface IProps {
-	
 }
 
-const Dropdown: React.FC<IProps> = ({  }) => {
+const Dropdown: React.FC<IProps> = ({ }) => {
 	return (
 		<StyledDropdown>
-			
+      <header>
+        Меню
+      </header>
+			<Button>Расписания звонков</Button>
+			<Button>Список предметов</Button>
 		</StyledDropdown>
 	)
 }
