b904334081de58cc1ed1e2bcf183af94536333d7
= <azazasdfg.com@gmail.com>
2023-04-11 15:07:52 +0300

    Add tests for LessonsStore

--- src/core/store/LessonsStore.ts
+++ src/core/store/LessonsStore.ts
@@ -19,33 +19,44 @@ class LessonsStore implements ILessonsStore {
 		return this._lessons.filter(l => l.uid !== 'hidden')
 	}
 
-	addLesson(newLesson: Omit<ILesson, 'uid'>) {
+	addLesson(newLesson: Omit<ILesson, 'uid'>, uid?: string) {
 		newLesson.title = capitalize(newLesson.title)
 		newLesson.teacher = capitalize(newLesson.teacher, true)
 
-		this._lessons.push({...newLesson, uid: nanoid(10)})
+		this._lessons.push({...newLesson, uid: uid || nanoid(10)})
 		console.log(toJS(this._lessons));
 	}
 
 	removeLesson(uid: string): boolean {
 		const indexToDelete = this._lessons.findIndex(lesson => lesson.uid === uid)
 		
-		if (indexToDelete != -1) {
-			const deletedLesson = this._lessons.splice(indexToDelete, 1)
-			console.log('Lesson deleted from store.', deletedLesson);
-			return deletedLesson.length === 1 
+		if (indexToDelete === -1) {
+			console.warn(`Can't remove.\nLesson with id "${uid}" not found.`)
+			return false
 		}
 		
-		console.warn(`Lesson with id "${uid}" not found.`)
-		return false
+		const deletedLesson = this._lessons.splice(indexToDelete, 1)
+		console.log('Lesson deleted from store.', deletedLesson);
+		return deletedLesson.length === 1
 	}
 
-	updateLesson(uid: string, newLesson: Omit<ILesson, 'uid'>) {
-		this._lessons = this._lessons.map(lesson => {
-			if (lesson.uid !== uid) return lesson
+	updateLesson(uid: string, newLesson: Omit<ILesson, 'uid'>): boolean {
+		const indexToUpdate = this._lessons.findIndex(l => l.uid === uid)
 
-			return { uid, ...newLesson }
-		})
+		if (indexToUpdate === -1) {
+			console.warn(`Can't update.\nLesson with id "${uid}" not found.`)
+			return false
+		}
+
+		this._lessons[indexToUpdate] = {
+			uid,
+			...newLesson,
+			title: capitalize(newLesson.title),
+			teacher: capitalize(newLesson.teacher, true)
+		}
+
+		console.log('Lesson modified successfully.')
+		return true
 	}
 }
 
---
+++ src/core/store/__tests__/LessonsStore.test.ts
@@ -0,0 +1,175 @@
+import LessonsStore from "../LessonsStore"
+
+
+describe('Testing LessonsStore', () => {
+	it('Tests default values', () => {
+		const lessonsStore = new LessonsStore()
+
+		expect(lessonsStore._lessons[0]).toEqual({
+			cabinet: '???',
+			teacher: '<Никто>',
+			title: '<Ничего>',
+			uid:'hidden'
+		})
+	})
+
+	it('Tests AddLesson action', () => {
+		const lessonsStore = new LessonsStore()
+		
+		// With uid passed
+		lessonsStore.addLesson({
+			cabinet: '202w',
+			teacher: 'Some Teacher\'s Name',
+			title: 'Math or whatever'
+		}, 'some-uid')
+
+		expect(lessonsStore._lessons[1]).toEqual({
+			cabinet: '202w',
+			teacher: 'Some Teacher\'s Name',
+			title: 'Math or whatever',
+			uid: 'some-uid'
+		})
+
+		// Without uid passed
+		lessonsStore.addLesson({
+			cabinet: '202w',
+			teacher: 'Some Teacher\'s Name 2',
+			title: 'Math or whatever 2'
+		})
+
+		expect(lessonsStore._lessons[2].cabinet).toBe('202w')
+		expect(lessonsStore._lessons[2].teacher).toBe('Some Teacher\'s Name 2')
+		expect(lessonsStore._lessons[2].title).toBe('Math or whatever 2')
+		expect(lessonsStore._lessons[2].uid).not.toBe('this :/')
+
+
+		// Test formatting
+		lessonsStore.addLesson({
+			cabinet: '202w',
+			teacher: 'not capitalized name',
+			title: 'math or whatever'
+		}, 'some-uid-2')
+
+		expect(lessonsStore._lessons[3]).toEqual({
+			cabinet: '202w',
+			teacher: 'Not Capitalized Name',
+			title: 'Math or whatever',
+			uid: 'some-uid-2'
+		})
+	})
+
+	it('Tests removeLesson action', () => {
+		const lessonsStore = new LessonsStore()
+
+		lessonsStore.addLesson({
+			cabinet: '202w',
+			teacher: 'Some Teacher\'s Name',
+			title: 'Math or whatever'
+		}, 'some-uid')
+		expect(lessonsStore._lessons).toHaveLength(2)
+		
+		// Nothing changed
+		let removedSuccuessfully: boolean | null = lessonsStore.removeLesson('cant-delelete-this')
+		expect(removedSuccuessfully).toBe(false)
+		expect(lessonsStore._lessons).toHaveLength(2)
+		
+		// Nothing changed again
+		removedSuccuessfully = null
+		removedSuccuessfully = lessonsStore.removeLesson(' ')
+		expect(removedSuccuessfully).toBe(false)
+		expect(lessonsStore._lessons).toHaveLength(2)
+
+		// Successfully removed
+		removedSuccuessfully = null
+		removedSuccuessfully = lessonsStore.removeLesson('some-uid')
+		expect(removedSuccuessfully).toBe(true)
+		expect(lessonsStore._lessons).toHaveLength(1)
+	})
+
+
+	it('Tests updateLesson action', () => {
+		const lessonsStore = new LessonsStore()
+
+		// Add new lesson
+		lessonsStore.addLesson({
+			cabinet: '202w',
+			teacher: 'Some Teacher\'s Name',
+			title: 'Math or whatever'
+		}, 'some-uid')
+		expect(lessonsStore._lessons).toHaveLength(2)
+		expect(lessonsStore._lessons[1]).toEqual({
+			cabinet: '202w',
+			teacher: 'Some Teacher\'s Name',
+			title: 'Math or whatever',
+			uid: 'some-uid'
+		})
+
+		// Modify it
+		let updatedSuccessfully: boolean | null = lessonsStore.updateLesson('some-uid', {
+			cabinet: '105-new',
+			teacher: 'New Teacher\'s Name',
+			title: 'New title'
+		})
+		expect(lessonsStore._lessons[1]).toEqual({
+			cabinet: '105-new',
+			teacher: 'New Teacher\'s Name',
+			title: 'New title',
+			uid: 'some-uid'
+		})
+		expect(updatedSuccessfully).toBe(true)
+
+		// Try modifying non-existing uid
+		updatedSuccessfully = null
+		updatedSuccessfully = lessonsStore.updateLesson('some-non-existing-uid', {
+			cabinet: 'whatever',
+			teacher: 'whatever',
+			title: 'whatever'
+		})
+		expect(updatedSuccessfully).toBe(false)
+		expect(lessonsStore._lessons[1]).toEqual({
+			cabinet: '105-new',
+			teacher: 'New Teacher\'s Name',
+			title: 'New title',
+			uid: 'some-uid'
+		})
+
+
+		// Test formatting
+		updatedSuccessfully = null
+		updatedSuccessfully = lessonsStore.updateLesson('some-uid', {
+			cabinet: '500g',
+			teacher: 'not capitalized name',
+			title: 'smol title'
+		})
+		expect(updatedSuccessfully).toBe(true)
+		expect(lessonsStore._lessons[1]).toEqual({
+			cabinet: '500g',
+			teacher: 'Not Capitalized Name',
+			title: 'Smol title',
+			uid: 'some-uid'
+		})
+	})
+
+	it('Tests that getters / public values return correct result', () => {
+		const lessonsStore = new LessonsStore()
+		
+		lessonsStore.addLesson({cabinet: '101w', teacher: 'Some Name', title: 'Some title'}, 'some-uid')
+
+		expect(lessonsStore._lessons).toHaveLength(2)
+		expect(lessonsStore._lessons[1]).toEqual({
+			cabinet: '101w',
+			teacher: 'Some Name',
+			title: 'Some title',
+			uid: 'some-uid'
+		})
+
+		// uid "hidden" is filtered
+		expect(lessonsStore.lessons).toHaveLength(1)
+		expect(lessonsStore.lessons[0]).toEqual({
+			cabinet: '101w',
+			teacher: 'Some Name',
+			title: 'Some title',
+			uid: 'some-uid'
+		})
+	})
+})
