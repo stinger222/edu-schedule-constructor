5b9db255ce32e52a1eaf874e998567606df326dd
Pavel <azazasdfg.com@gmail.com>
2023-10-04 14:40:14 +0300

    Add super raw init implementation of session login

--- server/index.js
+++ server/index.js
@@ -1,44 +1,71 @@
 
-const mongoose = require("mongoose")
+const cookieParser = require('cookie-parser')
 const express = require("express")
 const cors = require("cors")
+require("dotenv").config()
 
 const app = express()
-app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 
 const EXPRESS_PORT = process.env.BACKEND_PORT
-const MONGO_PORT = process.env.MONGO_PORT
-
-const UserSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-})
-
-const UserModel = mongoose.model("users", UserSchema)
 
 app.listen(EXPRESS_PORT, () => {
-  mongoose.connect(`mongodb://database/custom_schedule_db`)
-  
+  // mongoose.connect(`mongodb://database/custom_schedule_db`)
   console.log(`Server started on port ${EXPRESS_PORT}!!`)
 })
 
-app.get("/", async (req, res) => {
-  res.json({message: "As you can see, express kinda works"})
-
+app.get("/", async (req,  res) => {
+  res.json({message: "123"})
 })
 
-app.get("/create", async (req, res) => {
-  await UserModel.create({
-    age: 1,
-    name: "name"
+app.post("/auth/sign-in", async (req,  res) => {
+  console.log("======= NEW LOGIN =======\n", req.body)
+  
+  // clear session_id cookie in case if user already signed-in
+  res.clearCookie("session_id")
+
+  const newSessionId = Math.random()
+  await fetch("http://localhost:3003/sessions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email: req.body.email,
+      session_id: newSessionId+"",
+      epires_in: 2981342
+    })
   })
 
-  res.json({message: "Created! I beilve..."})
+  res.cookie("session_id", newSessionId)
+
+  res.status(200).json({
+    email: req.body.email
+  })
 })
 
-app.get("/get", async (req, res) => {
-  const x = await UserModel.find()
-  res.json(x.map(i => i.toJSON()))
+app.get("/auth/get-session", async (req, res) => {
+  const session_id = req.cookies.session_id
+
+  if (!session_id) {
+    res.status(401).end()
+    return
+  }
+
+  const sessions = await (await fetch("http://localhost:3003/sessions")).json()
+
+  const targetSession = sessions.find(s => {
+    return s.session_id === session_id
+  })
+  
+  if (!targetSession) {
+    res.clearCookie("session_id")
+    res.status(401).end()
+    return
+  }
+
+  res.status(200).json({email: targetSession.email})
 })
 
