75406ad1038f646a64d42cc2b5b63fb04d4a951b
Pavel <azazasdfg.com@gmail.com>
2023-10-11 23:31:18 +0300

    Add api endpoints to edit assembled schedules

--- client/src/pages/DebugPage/DebugPage.tsx
+++ client/src/pages/DebugPage/DebugPage.tsx
@@ -6,7 +6,7 @@ import Header from "../../components/smart/Header/Header"
 import Container from "../../components/containers/Container/Container"
 import { Cookies } from "react-cookie"
 import Button from "../../components/ui/Button/Button"
-import { IClassSchedule } from "../../core/types/types"
+import { IAssembledSchedule, IClassSchedule } from "../../core/types/types"
 
 const DebugPage = () => {
   const { authStore } = useContext(StoreContext)
@@ -24,8 +24,10 @@ const DebugPage = () => {
 
   const deleteClassWithIdInputRef = useRef<any>()
   const deleteClassScheduleWithIdInputRef = useRef<any>()
+  const deleteAssembledScheduleWithIdInputRef = useRef<any>()
   const updateClassWithIdInputRef = useRef<any>()
   const updateClassScheduleWithIdInputRef = useRef<any>()
+  const updateAssembledScheduleWithIdInputRef = useRef<any>()
 
   const fetchMe = async () => {
     try {
@@ -37,7 +39,7 @@ const DebugPage = () => {
     } catch(err) { return }
   }
 
-  // ============= Classes =============
+  // ============= "Add" Requests =============
   
   const addClass = async () => {
     try {
@@ -58,46 +60,6 @@ const DebugPage = () => {
       fetchMe()
     } catch(err) { return }
   }
-
-  const updateClassWithId = async () => {
-    try {
-       await ky.put(`http://localhost:3001/users/me/classes/${updateClassWithIdInputRef.current.value}`, {
-        credentials: "include",
-        body: JSON.stringify({
-          title: "(updated title) " + Math.random(), 
-          teacher: "(updated teacher) " + Math.random(),
-          cabinet: "(updated cabinet) " + Math.random()
-        }),
-        headers: {
-          "content-type": "application/json"
-        }
-      }).json()
-      fetchMe()
-    } catch(err) { return }
-  }
-
-  const deleteClassWithId = async () => {
-    try {
-      const data = await ky.delete(`http://localhost:3001/users/me/classes/${deleteClassWithIdInputRef.current.value}`, {
-        credentials: "include"
-      }).json()
-      console.log(`Trying to delete class with id:"${deleteClassWithIdInputRef.current.value}". Result: `, data)
-      fetchMe()
-      deleteClassWithIdInputRef.current.value = ""
-    } catch(err) { return }
-  }
-
-  const deleteAllClasses = async () => {
-    try {
-      const data = await ky.delete(`http://localhost:3001/users/me/delete-all-classes`, {
-        credentials: "include"
-      }).json()
-      console.log("Trying to delete all classes. Result: ", data)
-      fetchMe()
-    } catch(err) { return }
-  }
-
-  // ============= Class Schdules =============
 
   const addClassSchedule = async () => {
     try {
@@ -120,6 +82,47 @@ const DebugPage = () => {
     } catch(err) { return }
   }
 
+  const addAssembledSchedule = async () => {
+    try {
+      const data = await ky.post(`http://localhost:3001/users/me/assembled-schedules`, {
+        credentials: "include",
+        body: JSON.stringify({
+          name: "(new) some name",
+          days: [{
+            classIds: new Array(5).fill(0).map(i => Math.random().toFixed(3).toString()),
+            classScheduleId: Math.random().toFixed(3).toString()
+          }],
+          uid: Math.random().toString()
+        } as IAssembledSchedule),
+        headers: {
+          "content-type": "application/json"
+        }
+      }).json()
+
+      console.log("Trying to add new class schedule. Result: ", data)
+      fetchMe()
+    } catch(err) { return }
+  }
+
+  // ============= "Update" Requests =============
+
+  const updateClassWithId = async () => {
+    try {
+       await ky.put(`http://localhost:3001/users/me/classes/${updateClassWithIdInputRef.current.value}`, {
+        credentials: "include",
+        body: JSON.stringify({
+          title: "(updated title) " + Math.random(), 
+          teacher: "(updated teacher) " + Math.random(),
+          cabinet: "(updated cabinet) " + Math.random()
+        }),
+        headers: {
+          "content-type": "application/json"
+        }
+      }).json()
+      fetchMe()
+    } catch(err) { return }
+  }
+
   const updateClassSchedulesWithId = async () => {
     try {
       await ky.put(`http://localhost:3001/users/me/class-schedules/${updateClassScheduleWithIdInputRef.current.value}`, {
@@ -128,8 +131,7 @@ const DebugPage = () => {
         name: "(updated) class schedule name " + Math.random().toFixed(5),
         classes: [
           {start: `${+Math.random().toFixed(0)+1}:00`, end:`${+Math.random().toFixed(0)*2+2}:00`}
-        ],
-        uid: Math.random().toString()
+        ]
       } as IClassSchedule),
        headers: {
          "content-type": "application/json"
@@ -137,6 +139,38 @@ const DebugPage = () => {
      }).json()
      fetchMe()
    } catch(err) { return }
+  }
+
+  const updateAssembledSchedule = async () => {
+    try {
+      await ky.put(`http://localhost:3001/users/me/assembled-schedules/${updateAssembledScheduleWithIdInputRef.current.value}`, {
+       credentials: "include",
+       body: JSON.stringify({
+        name: "(updated) some new name",
+        days: [{
+          classIds: new Array(5).fill(0).map(i => Math.random().toFixed(3).toString()),
+          classScheduleId: Math.random().toFixed(3).toString()
+        }]
+      } as IAssembledSchedule),
+       headers: {
+         "content-type": "application/json"
+       }
+     }).json()
+     fetchMe()
+   } catch(err) { return }
+  }
+
+  // ============= "Delete By Id" Requests =============
+
+  const deleteClassWithId = async () => {
+    try {
+      const data = await ky.delete(`http://localhost:3001/users/me/classes/${deleteClassWithIdInputRef.current.value}`, {
+        credentials: "include"
+      }).json()
+      console.log(`Trying to delete class with id:"${deleteClassWithIdInputRef.current.value}". Result: `, data)
+      fetchMe()
+      deleteClassWithIdInputRef.current.value = ""
+    } catch(err) { return }
   }
 
   const deleteClassScheduleWithId = async () => {
@@ -149,13 +183,46 @@ const DebugPage = () => {
       deleteClassScheduleWithIdInputRef.current.value = ""
     } catch(err) { return }
   }
-  
+
+  const deleteAssembledScheduleWithId = async () => {
+    try {
+      const data = await ky.delete(`http://localhost:3001/users/me/assembled-schedules/${deleteAssembledScheduleWithIdInputRef.current.value}`, {
+        credentials: "include"
+      }).json()
+      console.log(`Trying to delete assembled schedule with id:"${deleteAssembledScheduleWithIdInputRef.current.value}". Result: `, data)
+      fetchMe()
+      deleteAssembledScheduleWithIdInputRef.current.value = ""
+    } catch(err) { return }
+  }
+
+  // ============= "Delete All" Requests =============
+
+  const deleteAllClasses = async () => {
+    try {
+      const data = await ky.delete(`http://localhost:3001/users/me/delete-all-classes`, {
+        credentials: "include"
+      }).json()
+      console.log("Trying to delete all classes. Result: ", data)
+      fetchMe()
+    } catch(err) { return }
+  }
+
   const deleteAllClassSchedules = async () => {
     try {
       const data = await ky.delete(`http://localhost:3001/users/me/delete-all-class-schedules`, {
         credentials: "include"
       }).json()
       console.log("Trying to delete all class schedules. Result: ", data)
+      fetchMe()
+    } catch(err) { return }
+  }
+
+  const deleteAllAssembledSchedules = async () => {
+    try {
+      const data = await ky.delete(`http://localhost:3001/users/me/delete-all-assembled-schedules`, {
+        credentials: "include"
+      }).json()
+      console.log("Trying to delete all assembled schedules. Result: ", data)
       fetchMe()
     } catch(err) { return }
   }
@@ -174,7 +241,7 @@ const DebugPage = () => {
           <Header.BurgerButton/>
         </Header>
       </Container>
-      <div style={{width: "70em", margin: "0 auto", display: "flex", justifyContent: "space-between"}}>
+      <div style={{width: "73em", margin: "0 auto", display: "flex", justifyContent: "space-between"}}>
         <div style={{flexBasis: "50%", height: "fit-content", borderRight: "2px solid lightgray", paddingRight: "2em"}}>
           <div>isAuthorized: <strong>{authStore.isSignedIn.toString()}</strong></div>
           <br/>
@@ -187,29 +254,48 @@ const DebugPage = () => {
           <br/>
         </div>
         
-        <div style={{height: "75vh", overflowY: "auto", fontSize: "0.8em", paddingRight: "1em"}}>
+        <div style={{height: "81vh", overflowY: "auto", fontSize: "0.8em", paddingRight: "1em"}}>
+          <h2>Classes controlls:</h2>
+          <br/>
           <Button onClick={addClass}>Add new class with random uid</Button>
           <br/><br/>
-          <Button onClick={() => updateClassWithId()}>Update class with passed uid</Button>
+          <Button onClick={updateClassWithId}>Update class with passed uid</Button>
           <input placeholder="uid:" style={inputStyles} ref={updateClassWithIdInputRef}/>
           <br/><br/>
-          <Button onClick={() => deleteClassWithId()}>Delete class with passed uid</Button>
+          <Button onClick={deleteClassWithId}>Delete class with passed uid</Button>
           <input placeholder="uid:" style={inputStyles} ref={deleteClassWithIdInputRef}/>
           <br/><br/>
           <Button onClick={deleteAllClasses}>Delete all classes</Button>
 
           <br/><br/>
           <br/><br/>
+          <h2>Class Schedules controlls:</h2>
+          <br/>
 
-          <Button onClick={() => addClassSchedule()}>Add class schedule with random uid</Button>
+          <Button onClick={addClassSchedule}>Add class schedule with random uid</Button>
           <br/><br/>
-          <Button onClick={() => updateClassSchedulesWithId()}>Update class schedule with uid</Button>
+          <Button onClick={updateClassSchedulesWithId}>Update class schedule with uid</Button>
           <input placeholder="uid:" style={inputStyles} ref={updateClassScheduleWithIdInputRef}/>
           <br/><br/>
-          <Button onClick={() => deleteClassScheduleWithId()}>Delete class schedule with uid</Button>
+          <Button onClick={deleteClassScheduleWithId}>Delete class schedule with uid</Button>
           <input placeholder="uid:" style={inputStyles} ref={deleteClassScheduleWithIdInputRef}/>
           <br/><br/>
-          <Button onClick={() => deleteAllClassSchedules()}>Delete all class schedules</Button>
+          <Button onClick={deleteAllClassSchedules}>Delete all class schedules</Button>
+
+          <br/><br/>
+          <br/><br/>
+          <h2>Assembled Schedules controlls:</h2>
+          <br/>
+
+          <Button onClick={addAssembledSchedule}>Add assembled schedule with random uid</Button>
+          <br/><br/>
+          <Button onClick={updateAssembledSchedule}>Update assembled schedule with uid</Button>
+          <input placeholder="uid:" style={inputStyles} ref={updateAssembledScheduleWithIdInputRef}/>
+          <br/><br/>
+          <Button onClick={deleteAssembledScheduleWithId}>Delete assembled schedule with uid</Button>
+          <input placeholder="uid:" style={inputStyles} ref={deleteAssembledScheduleWithIdInputRef}/>
+          <br/><br/>
+          <Button onClick={deleteAllAssembledSchedules}>Delete all assembled schedules</Button>
         </div>
 
       </div>
