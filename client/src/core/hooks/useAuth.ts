01808b19cf225874e05fdfb69e9929aa331968e9
Pavel <azazasdfg.com@gmail.com>
2023-10-22 02:21:23 +0300

    Add useAuth hook

---
+++ client/src/core/hooks/useAuth.ts
@@ -0,0 +1,24 @@
+import { useContext, useEffect, useState } from "react"
+import { StoreContext } from "../.."
+import { useNavigate } from "react-router-dom"
+import { useObserver } from "mobx-react"
+
+
+const useAuth = () => {
+  const { authStore } = useContext(StoreContext)
+  const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState(true)
+
+  const isSignedIn = useObserver(() => authStore.isSignedIn)
+
+  useEffect(() => {
+    setIsLoading(false)
+    if (isSignedIn === false) {
+      navigate("/auth/sign-in")
+    }
+  }, [authStore.isSignedIn])
+
+  return isLoading
+}
+
+export default useAuth
