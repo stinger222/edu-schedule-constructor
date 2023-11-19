b51b6db462155230747a8775c5bc72f82b69ca9f
Pavel <azazasdfg.com@gmail.com>
2023-11-19 16:39:06 +0300

    Move out login & register pages' shared logic

--- client/src/components/smart/AuthForm/AuthForm.styled.ts
+++ client/src/components/smart/AuthForm/AuthForm.styled.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components"
 
-export const StyledLoginPage = styled.div`
+export const StyledAuthForm = styled.div`
   
 `
--- client/src/pages/RegisterPage/RegisterPage.styled.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components"
-
-export const StyledRegisterPage = styled.div`
-  
-`
